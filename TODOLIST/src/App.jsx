46e5958d69b593b647efd7f4471e2970d580ca6a
SeanEArc <135284475+SeanEArc@users.noreply.github.com>
2025-05-12 18:15:02 -0500

    Add files via upload

---
+++ TODOLIST/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+import ToDoList from './components/ToDoList'
+
+function App() {
+  return (
+    <>
+      <div id="root">
+        <Header />
+      
+        <ToDoList />
+      </div>
+    </>
+  );
+};
+
+export default App;
