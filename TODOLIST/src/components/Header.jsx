8fdd8ba623ce9440748a3ed8639409f1937ca466
SeanEArc <135284475+SeanEArc@users.noreply.github.com>
2025-05-12 18:15:57 -0500

    Add files via upload

---
+++ TODOLIST/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h2>My To-Do List</h2>
+        </header>
+    );
+};
+
+export default Header;
---
+++ TODOLIST/src/components/ToDoList.jsx
@@ -0,0 +1,14 @@
+
+const ToDoList = () => {
+    return (
+        <div className="List">
+            <ul>
+                <li className="task">Learn React</li>
+                <li className="task">Build a project</li>
+                <li className="task">Read documentation</li>
+            </ul>
+        </div>
+    );
+};
+
+export default ToDoList;
